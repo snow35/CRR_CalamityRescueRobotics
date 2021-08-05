e54aa2bdc78a7ffef5f2953b1f0d44e0678f876b
신희설 <hijack0305@naver.com>
2021-08-05 16:04:07 +0900

    Hscore calculate function added

--- A_star.cpp
+++ A_star.cpp
@@ -1,9 +1,14 @@
 #include<iostream>
 #include<cmath> //sqrt 함수 사용
+#include<algorithm>
 
 #define ROW 10 //맵 가로 칸 수
 #define COL 10 //맵 세로 칸 수
 
+
+typedef pair<int, int> Pair;
+
+typedef pair<double, pair<int, int>> Pair_2;
 
 //각 노드별 정보 생성
 struct node
@@ -19,4 +24,19 @@ bool isValid(int x, int y)
 }
 
 //해당 노드가 막히지 않았는지 판단. 1 = 벽 / 0 = 길
-bool isUnblocked(int )
+bool isUnblocked(int grid[][COL], int x, int y) 
+{
+	if (grid[x][y] == 0)
+		return true;
+	else
+		return false;
+}
+
+//값 계산
+
+double Hscore(int x, int y, Pair dest)
+{
+	double dx = abs(x - dest.first);
+	double dy = abs(y - dest.second);
+	return 1 * (dx + dy) + (2 - 2 * 1) * std::min<dx, dy>; //diagonal distance 알고리즘 사용
+}
